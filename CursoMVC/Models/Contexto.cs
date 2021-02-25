46dbd0f8269ad658e302df1a723ae1434252f792
unknown <pablozagnoli@outlook.com>
2021-02-25 17:43:10 -0300

    Projeto MVC

---
+++ CursoMVC/Models/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC
+{
+    public class Contexto : DbContext
+    {
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder )
+        {
+            optionsBuilder.UseSqlServer(connectionString:@"Server = localhost\SQLEXPRESS; Database = CursoMvc; Trusted_Connection = True");
+        }
+    }
+}
